6df1b9b29206ff8a0bd6723d34b713e10eec5f77
cami-cohete <esquivca@mail.uc.edu>
2022-06-19 15:18:43 -0400

    Final Commit. Finished lab!

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,41 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+
+        double meters = 0;
+        double miles = 0;
+        double inches = 0;
+        double feet = 0;
+        String trash = "";
+
+        System.out.print("Please enter a measurement in meters: ");
+        if (in.hasNextDouble())
+        {
+            meters = in.nextDouble();
+            in.nextLine();
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("You entered an invalid integer: " + trash);
+            System.out.println("Please run the program again and enter a valid integer!");
+            System.exit(0);
+        }
+
+        miles = meters / 1609.344;
+        System.out.printf("The entered measurement is %.2f in miles.\n", miles);
+
+        inches = meters / 0.0254;
+        System.out.printf("The entered measurement is %.2f in inches.\n", inches);
+
+        feet = meters / 0.3048;
+        System.out.printf("The entered measurement is %.2f in feet.\n", feet);
 
     }
 }
